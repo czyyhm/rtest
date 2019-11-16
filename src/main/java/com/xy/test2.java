eb940a51e67406904019a21e4b4ebe226b92bd19
chen zeyu <630351260@qq.com>
2019-11-16 10:53:46 +0800

    diergeyewujinxingzhong

--- src/main/java/com/xy/test2.java
+++ src/main/java/com/xy/test2.java
@@ -3,5 +3,6 @@ package com.xy;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("dev diergeyewukaishi");
+        System.out.println("dev diergeyewujinxingzhong");
     }
 }
