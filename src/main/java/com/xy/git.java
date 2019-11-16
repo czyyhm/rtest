db692bd6b3e944cda8a4cf2aa79372eea7285a62
chen zeyu <630351260@qq.com>
2019-11-16 10:17:40 +0800

    yewuwancheng yewuyihebing

--- src/main/java/com/xy/git.java
+++ src/main/java/com/xy/git.java
@@ -4,5 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("sss");
+        System.out.println("yewukaishi");
+        System.out.println("yewujinxingzhong");
+        System.out.println("yewuwancheng");
     }
 }
