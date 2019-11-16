403424808b9aaa58f1390145e70001c2e2566f72
chen zeyu <630351260@qq.com>
2019-11-16 10:53:04 +0800

    diyigeyewujinxingzhong

--- src/main/java/com/xy/test.java
+++ src/main/java/com/xy/test.java
@@ -3,5 +3,6 @@ package com.xy;
 public class test {
     public static void main(String[] args) {
         System.out.println("dev diyigeyewukaishi");
+        System.out.println("dev diyigeyewujinxingzhong");
     }
 }
