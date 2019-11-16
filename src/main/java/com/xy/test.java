febc52cbeced26999cfb238c63dd15fea747863e
chen zeyu <630351260@qq.com>
2019-11-16 10:51:41 +0800

    gerenlianggexindeyewuhebingtijiao wancheng

--- src/main/java/com/xy/test.java
+++ src/main/java/com/xy/test.java
@@ -3,5 +3,7 @@ package com.xy;
 public class test {
     public static void main(String[] args) {
         System.out.println("dev diyigeyewukaishi");
+        System.out.println("dev diyigeyewujinxingzhong");
+        System.out.println("dev diyigeyewuwancheng");
     }
 }
---
+++ src/main/java/com/xy/test2.java
@@ -0,0 +1,9 @@
+package com.xy;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("dev diergeyewukaishi");
+        System.out.println("dev diergeyewujinxingzhong");
+        System.out.println("dev diergeyewuwancheng");
+    }
+}
